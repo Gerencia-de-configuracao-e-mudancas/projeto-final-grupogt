4e09ad605261e1767233eb1178ee47c9a45e2405
victorivis <knightdragonred@gmail.com>
2023-11-28 22:57:21 -0300

    Adicao de editar e apagar no localStorage e correcao do grafico de linhas

--- js/local-storage.js
+++ js/local-storage.js
@@ -16,3 +16,37 @@ function armazenarElemento(objeto){
         localStorage.financas = JSON.stringify([objeto]);
     }
 }
+
+//Trata as posicoes comecando do 0
+function editarElemento(objeto, posicao){
+    let listaElementos = receberElemento();
+    if(listaElementos){
+        if(posicao < listaElementos.length && posicao > -1){
+            //console.log("Edição possível");
+            listaElementos[posicao] = objeto;
+            localStorage.financas = JSON.stringify(listaElementos);   
+        }
+        else{
+            console.log("Tentando editar posicao que não existe");
+        }
+    }
+    else{
+        console.log("Tentando editar com a lista vazia");
+    }
+}
+
+function apagarElemento(posicao){
+    let listaElementos = receberElemento();
+    if(listaElementos){
+        if(posicao < listaElementos.length && posicao > -1){
+            listaElementos.splice(posicao, 1);
+            localStorage.financas = JSON.stringify(listaElementos);   
+        }
+        else{
+            console.log("Tentando apagar posicao que não existe");
+        }
+    }
+    else{
+        console.log("Tentando apagar com a lista vazia");
+    }
+}
