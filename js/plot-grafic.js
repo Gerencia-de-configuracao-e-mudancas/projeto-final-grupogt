fd2627db3608e071e58c678014a47bae28c70861
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-12-07 16:24:27 -0300

    adicao de grafico de barras

--- js/plot-grafic.js
+++ js/plot-grafic.js
@@ -1,22 +1,12 @@
 //Script dedicado as configurações dos gráficos
 
 //Os únicos parâmetros obrigatórios são id, valoresX e valoresX. Todos os outros tem valores padrões
-function criarGrafico(id, valoresX, valoresY, tipo, cores, titulo, legenda){
-    tipo = tipo ? tipo : "line";
-    titulo = titulo ? titulo : "Despesas por categoria";
+function criarGrafico(id, valoresX, valoresY, cores, titulo, legenda) {
     let mostrarTitulo;
+    cores = cores ? cores : ["rgb(2, 149, 206)", "rgb(80, 174, 83)", "rgb(244, 66, 55)"];
+    legenda = valoresX;
+    mostrarTitulo = true;
 
-    if(tipo == "line"){
-        //valoresY = Array.from(valoresY, (y) => Number(y));
-        cores = cores ? cores : "blue";
-        legenda = legenda ? legenda : "Gastos";
-        mostrarTitulo = false;
-    }
-    else{
-        cores = cores ? cores : ["rgb(2, 149, 206)", "rgb(80, 174, 83)","rgb(244, 66, 55)", "green", "purple"];
-        legenda = valoresX;
-        mostrarTitulo = true;
-    }
     new Chart(id, {
         type: 'doughnut',
         data: {
@@ -40,4 +30,52 @@ function criarGrafico(id, valoresX, valoresY, tipo, cores, titulo, legenda){
     });
 }
 
-criarGrafico("myChart", ["Saldo", "Receita", "Despesas"], ["2300.20", "430.00", "523.60"], "pie");
+//Plot destinado ao grafico em barra
+function criarGraficoBarra(id, valoresX, valoresY, cores, legenda) {
+    let mostrarTitulo;
+    cores = cores ? cores : ["rgb(80, 174, 83)", "rgb(244, 66, 55)"];
+    legenda = valoresX;
+    mostrarTitulo = true;
+
+    new Chart(id, {
+        type: 'bar',
+        data: {
+            labels: valoresX,
+            datasets: [{
+                label: '# of Votes',
+                data: valoresY,
+                backgroundColor: cores,
+                lineTension: 1,
+                borderWidth: 1,
+                borderRadius: 100 ,
+    
+            }]
+        },
+        options: {
+            
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            //Ocultando eixos e legendas
+            scales: {
+                x: {
+                    display: false, 
+                },
+                y: {
+                    display: false,
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+}
+
+criarGrafico("myChart", ["Saldo", "Receita", "Despesas"], ["2300.20", "430.00", "523.60"]);
+criarGraficoBarra("myChartBar", ["Receita", "Despesas"], ["430.00", "523.60"])
