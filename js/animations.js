e7d38f74d9852a90a47fbca80d0e427ca9d17612
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-12-09 14:02:40 -0300

    ajueste no timer

--- js/animations.js
+++ js/animations.js
@@ -7,7 +7,25 @@ document.querySelector("#form").addEventListener('submit', e => {
     const pass = document.querySelector('#senha').value;
 
     if (user === 'Dom Vito' && pass === 'corleone') {
-        window.location.href = './SRC/main.html';
+        let timerInterval;
+        Swal.fire({
+            title: "Carregando",
+            html: "Aguarde",
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading();
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+                window.location.href = './SRC/main.html';
+            }
+        }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log("I was closed by the timer");
+            }
+        });
     }
     else {
         Swal.fire({
