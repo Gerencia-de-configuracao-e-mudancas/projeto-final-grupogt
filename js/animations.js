84fca960ed925594ffdd6ba646e50b6e2ec2cdeb
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-12-09 14:14:48 -0300

    ajueste no login

--- js/animations.js
+++ js/animations.js
@@ -6,32 +6,31 @@ document.querySelector("#form").addEventListener('submit', e => {
     const user = document.querySelector('#user').value;
     const pass = document.querySelector('#senha').value;
 
-    if (user === 'Dom Vito' && pass === 'corleone') {
-        let timerInterval;
-        Swal.fire({
-            title: "Carregando",
-            html: "Aguarde",
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: () => {
-                Swal.showLoading();
-            },
-            willClose: () => {
-                clearInterval(timerInterval);
+    let timerInterval;
+    Swal.fire({
+        title: "Carregando",
+        html: "Aguarde",
+        timer: 3000,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+            if (user === 'Dom Vito' && pass === 'corleone') {
                 window.location.href = './SRC/main.html';
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Usuário ou senha incorretos",
+                });
             }
-        }).then((result) => {
-            /* Read more about handling dismissals below */
-            if (result.dismiss === Swal.DismissReason.timer) {
-                console.log("I was closed by the timer");
-            }
-        });
-    }
-    else {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Usuário ou senha incorretos",
-        });
-    }
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+        }
+    });
+
 })
